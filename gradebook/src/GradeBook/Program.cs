7edac778b09d98f5cbb547c78c7638777f39e551
jromerooo2 <75763058+jromerooo2@users.noreply.github.com>
2020-12-31 01:25:00 -0600

    Gave it values to test it out

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -12,14 +12,14 @@ namespace GradeBook
                             //Main class
             var book = new Book("Math Book");
              System.Console.WriteLine("Before Grades:");
-            book.AddGrades();
-            book.AddGrades();
-            book.AddGrades();
-            book.AddGrades();
-            book.AddGrades();
-            book.AddGrades();
-            System.Console.WriteLine();
-            book.RemoveGrades();
+            book.AddGrades(2.4);
+            book.AddGrades(4.5);
+            book.AddGrades(100);
+            book.AddGrades(48.9);
+            book.AddGrades(55.4);
+            book.AddGrades(33);
+            System.Console.WriteLine("After Grades");
+            book.RemoveGrades(3);
            /*  book.RemoveGrades(); */
             book.showStats();
                          
