8b56727a1e313f0511a135fc02bdfe1396f93dde
jromerooo2 <75763058+jromerooo2@users.noreply.github.com>
2020-12-31 01:19:11 -0600

    created book.css
    and added empty methods

---
+++ gradebook/src/GradeBook/book.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+
+    class Book
+    {
+
+        //Add Grades.  
+
+        public void AddGrades(double added)
+        {
+               
+          
+        }
+        //Remove Grades Method
+                public void RemoveGrades(int removed)
+        {
+
+                             
+
+        }
+         
+                
+        //Shows statistics for the Highest, Lowest n Average Grades
+        public void showStats()
+        {
+
+        }
+
+
+
+        public  Book(string name)
+        {
+
+                
+        }
+
+        List<double> grades;
+              
+
+        }
+}
